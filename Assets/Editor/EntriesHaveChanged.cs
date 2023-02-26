997a644e97271f216b7f084e64e0c9ae9b65bd84
f-fagua <felipe.fagua@unity3d.com>
2023-02-25 22:17:07 -0500

    first working version of the rule

--- Assets/Editor/EntriesHaveChanged.cs
+++ Assets/Editor/EntriesHaveChanged.cs
@@ -6,30 +6,24 @@ using UnityEditor;
 using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEditor.AddressableAssets.Settings.GroupSchemas;
+using UnityEditor.Build.Pipeline;
 using UnityEngine;
 
 public class EntriesHaveChanged : UnityEditor.AddressableAssets.Build.AnalyzeRules.AnalyzeRule
 {
-    private const string kLastDataFileName = "LastBuildData.asset";
-    
     private LastBuildData m_LastBuildData;
     
     public override bool CanFix
     {
-        get { return false;}
+        get => false;
         set { }
     }
 
-    public override string ruleName
-    {
-        get { return "Check which assets have changed"; }
-    }
-
-    private string LastBuildDataPath => Path.Combine("Assets", "AddressableAssetsData", "LastBuildData", kLastDataFileName );
+    public override string ruleName => "Check which assets have changed";
 
     public override List<AnalyzeResult> RefreshAnalysis(AddressableAssetSettings settings)
     {
-        m_LastBuildData = AssetDatabase.LoadAssetAtPath<LastBuildData>(LastBuildDataPath);
+        m_LastBuildData = AssetDatabase.LoadAssetAtPath<LastBuildData>(LastBuildData.DefaultAssetPath);
         
         List<AnalyzeResult> results = new List<AnalyzeResult>();
         foreach (var group in settings.groups)
@@ -42,9 +36,9 @@ public class EntriesHaveChanged : UnityEditor.AddressableAssets.Build.AnalyzeRul
                 var hasChanged = m_LastBuildData.HasChanged(e.AssetPath, out var isNewAsset, out var oldHash, out var newHash);
                 
                 if (isNewAsset)
-                    results.Add(new AnalyzeResult{resultName = "[New Asset] " + group.Name + kDelimiter + e.address, severity = MessageType.Info});
+                    results.Add(new AnalyzeResult{resultName = "New Assets" + kDelimiter + group.Name + kDelimiter + e.address, severity = MessageType.Warning});
                 else if(hasChanged)
-                    results.Add(new AnalyzeResult{resultName = group.Name + kDelimiter + e.address + $" old hash {oldHash}, new hash {newHash}", severity = MessageType.Error});
+                    results.Add(new AnalyzeResult{resultName = "Changed Assets" + kDelimiter + group.Name + kDelimiter + e.address + $" from {oldHash} to {newHash}", severity = MessageType.Error});
             }
         }
         
--- Assets/Editor/LastBuildData.cs
+++ Assets/Editor/LastBuildData.cs
@@ -1,14 +1,32 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.Build.Pipeline;
+using UnityEditor.Build.Pipeline.Interfaces;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LastBuildData", menuName = "Addressables/LastBuildData", order = 1)]
-public class LastBuildData : ScriptableObject
+public class LastBuildData : ScriptableObject, ISerializationCallbackReceiver
 {
-    
-    
-    private Dictionary<string, string> m_Entries = new Dictionary<string, string>();
+    private const string kLastDataFileName = "LastBuildData.asset";
+    public static string DefaultAssetPath => Path.Combine("Assets", "AddressableAssetsData", "LastBuildData", kLastDataFileName );
 
+    [SerializeField]
+    private List<string> m_EntryPaths;
+    
+    [SerializeField]
+    private List<string> m_EntryHashes;
+
+    private Dictionary<string, string> m_Entries = new Dictionary<string, string>();
+    
+    public void SetData(List<string> entryPaths, List<string> entryHashes)
+    {
+        m_EntryPaths = entryPaths;
+        m_EntryHashes = entryHashes;
+        RebuildDictionary();
+    }
+    
     public bool HasChanged(string assetPath, out bool isNewAsset, out string oldHash, out string newHash)
     {
         oldHash = "";
@@ -19,8 +37,79 @@ public class LastBuildData : ScriptableObject
         if (isNewAsset)
             return true;
 
-        var assetGUID = AssetDatabase.AssetPathToGUID(assetPath);
-        newHash = AssetDatabase.GetAssetDependencyHash(assetGUID).ToString();
+        newHash = AssetDatabase.GetAssetDependencyHash(assetPath).ToString();
         return newHash != oldHash;
     }
+
+    public static void CreateLastBuildDataAsset()
+    {
+        LastBuildData defaultAsset = ScriptableObject.CreateInstance<LastBuildData>();
+        AssetDatabase.CreateAsset(defaultAsset, DefaultAssetPath);
+    }
+
+    public void OnBeforeSerialize()
+    {
+        m_EntryPaths.Clear();
+        m_EntryHashes.Clear();
+        
+        foreach (var kvp in m_Entries)
+        {
+            m_EntryPaths.Add(kvp.Key);
+            m_EntryHashes.Add(kvp.Value);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        RebuildDictionary();
+    }
+
+    private void RebuildDictionary()
+    {
+        m_Entries = new Dictionary<string, string>();
+
+        for (int i = 0; i < m_EntryPaths.Count; i++)
+        {
+            m_Entries.Add(m_EntryPaths[i], m_EntryHashes[i]);
+        }
+    }
 }
+
+[InitializeOnLoad]
+class RegisterBuildCallbacks    {
+    static RegisterBuildCallbacks()
+    {
+        ContentPipeline.BuildCallbacks.PostScriptsCallbacks -= OnBuildCompleted;
+        ContentPipeline.BuildCallbacks.PostScriptsCallbacks += OnBuildCompleted;
+    }
+
+    private static ReturnCode OnBuildCompleted(IBuildParameters buildParameters, IBuildResults buildResults)
+    {
+        Debug.Log("OnBuildCompleted");
+
+        if (!File.Exists(LastBuildData.DefaultAssetPath))
+            LastBuildData.CreateLastBuildDataAsset();
+
+        var lastBuildData = AssetDatabase.LoadAssetAtPath<LastBuildData>(LastBuildData.DefaultAssetPath);
+
+        var entryPaths = new List<string>();
+        var entryHashes = new List<string>();
+
+        foreach (var group in AddressableAssetSettingsDefaultObject.Settings.groups)
+        {
+            foreach (var entry in group.entries)
+            {
+                entryPaths.Add(entry.AssetPath);
+                entryHashes.Add(AssetDatabase.GetAssetDependencyHash(entry.AssetPath).ToString());
+            }
+        }
+
+        lastBuildData.SetData(entryPaths, entryHashes);
+        
+        AssetDatabase.SaveAssets();
+        
+        AssetDatabase.ForceReserializeAssets(new[] {LastBuildData.DefaultAssetPath});
+        
+        return ReturnCode.Success;
+    }
+}    
